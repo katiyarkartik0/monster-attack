1d58e009c45c9af1a18aa866774c65713681c0ef
katiyarkartik0 <katiyarkartik0@gmail.com>
2022-07-09 18:14:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css"
+function App() {
+  return (
+    <>
+    <div className="box1">
+      <div className="box2">
+        <div className="healthBox"></div>
+        <div className="moveBox"></div>
+        <div className="recordBox"></div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
